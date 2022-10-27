f0cac4550a61aaa421affd89956600e3e4ae43c2
Carl Fredrick <carlfredrick1708@gmail.com>
2022-10-27 11:55:20 -0600

    added shutter speed functionality
    the Interactable class handles the object that is changed via low/high shutter speed. the Movable class allows an object to move from its current position to an end position.

---
+++ Assets/Scripts/Movable.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Test class that moves to and from start/end positions at a set speed.
+public class Movable : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+
+    [SerializeField]
+    private Transform endPos;
+
+    public bool isMoving = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+            transform.position = Vector3.MoveTowards(transform.position, endPos.position, speed * Time.deltaTime);
+    }
+}
