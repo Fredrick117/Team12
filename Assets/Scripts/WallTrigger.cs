cc6eca2e1b4b3687c2cb2f51642cae9825d5fe8f
Zephyrus20 <u1429413@umail.utah.edu>
2022-11-09 14:24:18 -0700

    added vr rig and wall trigger in level 3

---
+++ Assets/Scripts/WallTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject wall1, wall2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wall1.GetComponent<objectMove>().enabled = false;
+        wall2.GetComponent<objectMove>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            wall1.GetComponent<objectMove>().enabled = true;
+            wall2.GetComponent<objectMove>().enabled = true;
+        }
+    }
+}
